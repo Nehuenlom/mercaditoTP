9e7d30c12096c7a0c2d6fb226eae4c6646dff65f
NehuenLom <93491147+Nehuenlom@users.noreply.github.com>
2022-04-29 20:18:31 -0300

    hasta aca n

---
+++ src/pag2.ts
@@ -0,0 +1,5 @@
+let productos: string[] = ["yerba", "Azucar", "fernet","coca-cola", "chocolatada"];
+let precios: number[] = [120, 105, 850, 250, 200];
+let stock: number[] = [200, 120, 50, 300, 20];
+
+for (let i = 0 < stock.length; i++)
